a1e9b612272736d07b4eec41e5c52416e86f8325
Evgeny Potorochin <37625988+Olidood@users.noreply.github.com>
2020-03-22 17:06:22 +0300

    Update givemyaps.js

--- server/CleanNFM-master1/CleanNFM-master1/services/api/v1/users/givemyaps.js
+++ server/CleanNFM-master1/CleanNFM-master1/services/api/v1/users/givemyaps.js
@@ -1,11 +1,11 @@
 module.exports = async function(f, opts){
     const Mongoose = require("mongoose");
     const Schema = Mongoose.Schema;
-    const Search = Mongoose.model("search");
+    const MyAps = Mongoose.model("myaps");
 
 
    f.get('/givemyaps/:login', async (req, res)=> {
-      const search = await Search.find({contacts:{emailadr: req.params.login}}).exec();
+      const search = await MyAps.find({ownerlogin: req.params.login}).exec();
           if(search) res.send({code:200, search});
           else res.send({code:400});
   })
