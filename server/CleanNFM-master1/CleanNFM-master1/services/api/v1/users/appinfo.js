5632d6f59b661b8e0f3209b7e7baa25e999554fb
Evgeny Potorochin <37625988+Olidood@users.noreply.github.com>
2020-03-22 17:00:01 +0300

    Repush

--- server/CleanNFM-master1/CleanNFM-master1/services/api/v1/users/appinfo.js
+++ server/CleanNFM-master1/CleanNFM-master1/services/api/v1/users/appinfo.js
@@ -1,11 +1,11 @@
 module.exports = async function(f, opts){
     const Mongoose = require("mongoose");
-    const MyAps = Mongoose.model("myaps");
+    const Search = Mongoose.model("search");
 
     f.get('appinfo/:appartmentsid', async (req, res)=>{
         let user = new User({appartmentsid: req.params.appartmentsid});
 
-        MyAps.findOne({appartmentsid: req.params.appartmentsid}).select('floorap allfloors sqware cost roomsnum town street house metro renttype contacts description').exec(function (err, pass) {
+        User.findOne({appartmentsid: req.params.appartmentsid}).select('floorap allfloors sqware cost roomsnum town street house metro renttype contacts description').exec(function (err, pass) {
             if(err) res.send({code:400});
             else {
 
